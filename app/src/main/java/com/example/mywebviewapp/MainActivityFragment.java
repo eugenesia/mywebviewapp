6b3051f0da8d3cdc216bdb9f51fdfe362afc66b1
Eugene Sia <eugene@eugenesia.co.uk>
2016-02-13 03:39:47 +0000

    Get WebView from View

--- app/src/main/java/com/example/mywebviewapp/MainActivityFragment.java
+++ app/src/main/java/com/example/mywebviewapp/MainActivityFragment.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+  private WebView mWebView;
 
   public MainActivityFragment() {
   }
@@ -17,6 +20,10 @@ public class MainActivityFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_main, container, false);
+
+    View v = inflater.inflate(R.layout.fragment_main, container, false);
+    mWebView = (WebView) v.findViewById(R.id.activity_main_webview);
+
+    return v;
   }
 }
