e252716c644b48b627c290cd60e8c9cd60616bec
Eugene Sia <eugene@eugenesia.co.uk>
2016-02-16 22:45:45 +0000

    Override WebViewClient to control link opening

---
+++ app/src/main/java/com/example/mywebviewapp/MyAppWebViewClient.java
@@ -0,0 +1,11 @@
+package com.example.mywebviewapp;
+
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Eugene on 16/02/16.
+ * Override WebViewClient to control when to open links in a browser,
+ * and when internally.
+ */
+public class MyAppWebViewClient extends WebViewClient {
+}
